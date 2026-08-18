4c685ca8911628dacf4472d3a726efee014862ad
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-08-19 00:05:04 +0600

    chore: add firebase ignore patterns and remove unused error handler import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:genai_labs/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_eme_base/flutter_eme_base.dart';
-import 'package:flutter_eme_base/utils/error_handler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
