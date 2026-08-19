a0cbe8fbdd1d83ab96c9106a3662232a56549011
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-08-20 03:54:46 +0600

    new build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2009484559" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3645310412" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
